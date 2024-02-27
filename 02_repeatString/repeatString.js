fa0370d8463b0f2688e195981be348d1bfbfed6a
Rob England <robenng@icloud.com>
2024-02-27 18:31:34 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, rep) {
+  if (rep < 0) return 'ERROR';
+  
+  let repStr = '';
 
+  for (let i = 0; i < rep; i++) {
+    repStr += str;
+  }
+
+  return repStr;
 };
 
 // Do not edit below this line
