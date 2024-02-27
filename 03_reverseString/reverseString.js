13c422515149f40b2fe14001f19d5683830483e8
Rob England <robenng@icloud.com>
2024-02-27 18:35:54 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const chars = str.split('');
+  const reversed = chars.reverse();
+  
+  return reversed.join('');
 };
 
 // Do not edit below this line
