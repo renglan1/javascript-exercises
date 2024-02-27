3d0d5094eac123ee9e4686b9033ed85bb3aba0e2
Rob England <robenng@icloud.com>
2024-02-27 18:52:13 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elems) {
+  for(let i = 0; i < elems.length; i++) {
+    array = array.filter((elem) => elem !== elems[i]);
+  }
 
+  return array;
 };
 
 // Do not edit below this line
