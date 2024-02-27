463752abc7cb36cb03f26f21746023259867456c
Rob England <robenng@icloud.com>
2024-02-27 19:06:41 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  let sum = first;
+  let next;
 
+  if (first < 0 || last < 0) {
+    return 'ERROR';
+  }
+
+  if (typeof(first) !== 'number' || typeof(last) !== 'number') {
+    return 'ERROR';
+  }
+
+  if (first <= last) {
+    next = first + 1;
+
+    for (let i = first; i < last; i++) {
+      sum += next++;
+    }
+  }
+  else {
+    next = first - 1;
+
+    for (let i = first; i > last; i--) {
+      sum += next--;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
