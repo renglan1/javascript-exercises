23721a31054609af99950948a83ca0ce4adf7afb
Rob England <robenng@icloud.com>
2024-03-04 15:49:24 -0400

    Refactor leap years to use more efficient algorithm

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,5 @@
 const leapYears = function(year) {
-  if(year%4 === 0) {
-    if(year%100 === 0) {
-      if(year%400 === 0) {
-        return true;
-      }
-      return false;
-    }
-    return true;
-  }
-
-  return false;
+  return year%4 === 0 && (year % 100 !== 0 || year%400 === 0);
 };
 
 // Do not edit below this line
