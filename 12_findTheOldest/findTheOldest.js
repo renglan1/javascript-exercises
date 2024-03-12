4c619468325a887be922f33fd11c97481f3f212f
Rob England <robenng@icloud.com>
2024-03-12 17:40:29 -0300

    Complete exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((prevPerson, curPerson) => {
+    prevAge = (prevPerson.yearOfDeath ? prevPerson.yearOfDeath : new Date().getFullYear()) - prevPerson.yearOfBirth;
+    curAge = (curPerson.yearOfDeath ? curPerson.yearOfDeath : new Date().getFullYear()) - curPerson.yearOfBirth;
 
+    return prevAge > curAge ? prevPerson : curPerson;
+  });
 };
 
 // Do not edit below this line
