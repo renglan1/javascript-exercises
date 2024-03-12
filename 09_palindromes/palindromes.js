a7f0d3f39b7cfd5587ffd474f8a0bd0898a66209
Rob England <robenng@icloud.com>
2024-03-12 17:12:12 -0300

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const chars = string.toLowerCase().split("");
+  const saniChars = chars.filter(
+    (char) =>
+      char !== "," &&
+      char !== " " &&
+      char !== "!" &&
+      char !== "." &&
+      char !== "?"
+  );
+  return saniChars.join("") == saniChars.reverse().join("");
 };
 
 // Do not edit below this line
